4954a51e6e61d27bf991e37c332b099c530ebe59
Seth Wax <sethlwax@gmail.com>
2019-01-24 11:51:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,7 @@ class Student
 
   attr_accessor :id, :name, :grade
 
-  def initialize(:id = nil, :name =, :grade =)
+  def initialize(id = nil, name, grade)
     @id = id
     @name = name
     @grade = grade
